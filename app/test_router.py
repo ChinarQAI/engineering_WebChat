c8df4edcba716c434b73650f9c6212b1b4388ec5
alquantumai <owaisibnmushtaq@gmail.com>
2025-01-23 11:54:52 +0530

    first test on ci-cd

--- app/test_router.py
+++ app/test_router.py
@@ -1,6 +1,13 @@
 import pytest
 from fastapi.testclient import TestClient
-from router import app  # Ensure 'router' matches your application filename
+import sys
+import os
+
+# Add the project directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from router import app  
+
 
 # Create a test client for the FastAPI app
 client = TestClient(app)
